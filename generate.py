2a316f5e3a2f596cd0749a30522de1c0e13a3f12
Caio Ramos <caioramos97@gmail.com>
2018-05-16 11:16:39 -0300

    Script for generating matrixes (correct format)

---
+++ generate.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sys
+
+row = int(sys.argv[1])
+col = int(sys.argv[2])
+n   = int(sys.argv[3])
+
+print str(row) + " " + str(col)
+
+for _ in xrange(n):
+    #generate 'i j number'
+
