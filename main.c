2276252a24fa69e33a54212b51fbdc944830df93
Caio Henrique Silva Ramos <caiohsramos@ruby.linux.ime.usp.br>
2018-05-16 10:45:00 -0300

    Some changes to incorrect use and prints

--- main.c
+++ main.c
@@ -4,16 +4,20 @@
 
 int main(int argc, char *argv[]) {
 	if(argc < 5) {
-		printf("Incorrect use: ./main <imp> <A> <B> C>\n");
+		printf("Incorrect use.\n\tUsage: ./main <imp> <A> <B> C>\n");
 		return 1;
 	}
 	switch(argv[1][0]) {
 		case 'p':
+			printf("Chosen alg: Pthreads.\n");
 			multiply(argv[4], argv[2], argv[3], &pt);
 			break;
 		case 'o':
+			printf("Chosen alg: OpenMP.\n");
 			multiply(argv[4], argv[2], argv[3], &omp);
 			break;
+		default:
+			printf("%c is an invalid option\n", argv[1][0]);
 	};
 	return 0;
 }
