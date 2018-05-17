b720001e64ed1b1af39282e5a71e51d3c6fc36a9
Caio Ramos <caioramos97@gmail.com>
2018-05-17 13:32:56 -0300

    Finished generate matrix script

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys
+import sys, random
 
 row = int(sys.argv[1])
 col = int(sys.argv[2])
@@ -10,4 +10,8 @@ print str(row) + " " + str(col)
 
 for _ in xrange(n):
     #generate 'i j number'
-
+    i = random.randint(1, row)
+    j = random.randint(1, col)
+    val = random.random() + 1.0
+    print "%d %d %f" % (i, j, val)
+    
